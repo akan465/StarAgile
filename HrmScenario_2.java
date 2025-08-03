beb5e292180dcfe55ec33777919d452861d52490
akan465 <akhare465@gmail.com>
2025-08-03 15:58:33 +0530

    Add files via upload

---
+++ HrmScenario_2.java
@@ -0,0 +1,98 @@
+package com.WebdriverDemo3;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HrmScenario_2 {
+	
+	static @FindBy(name  = "username")WebElement loginPage_uname;
+	static @FindBy(name  = "password")WebElement loginPage_pass;
+	static @FindBy(xpath = "//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")WebElement loginPage_submit;
+	static @FindBy(xpath = "//*[@href='/web/index.php/admin/viewAdminModule']")WebElement admin_option1;
+	static @FindBy(xpath = "//ul[@class='oxd-main-menu']/li")List<WebElement> admin_leftSideMenuItems;
+	static @FindBy(xpath = "//span[text()='Admin']")WebElement admin_MenuOption;
+	static @FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[2]")WebElement searchByUname_SearchBox;
+	static @FindBy(xpath = "//button[normalize-space()='Search']")WebElement searchByUname_searchBtn;
+	static @FindBy(xpath = "//div[@class='oxd-table-body']/div")List<WebElement> resultRows;
+	static @FindBy(xpath = "//div[@class='oxd-table-filter-area']//div[2]//div[1]//div[2]//div[1]//div[1]//div[2]//i[1]")WebElement searchbyRole_userRoleDropdown;
+	static @FindBy(xpath = "//div[@class='oxd-select-option']/span[text()='Admin']")WebElement searchbyRole_adminOption;
+	static @FindBy(xpath = "//button[normalize-space()='Reset']")WebElement resetButton;
+	static @FindBy(xpath = "//label[text()='Status']/../following-sibling::div//div[@class='oxd-select-text-input']")WebElement searchByStatus_dropdown;
+	static @FindBy(xpath = "//div[text()='Enabled']")WebElement searchByStatus_enabled;
+//using PageFactory
+	static WebDriver driver;
+
+
+	public static void main(String[] args) {
+		driver= new ChromeDriver();
+		PageFactory.initElements(driver,HrmScenario_2.class );
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		loginPage();
+		admin();
+		searchByUname("Admin");
+		searchbyRole();
+		searchByStatus("Enabled");
+		driver.quit();
+
+	}
+	
+	public static void loginPage() {
+		loginPage_uname.sendKeys("Admin");
+		loginPage_pass.sendKeys("admin123");
+		loginPage_submit.click();
+
+		
+	}
+	
+	public static void admin() {
+		System.out.println("--------------------Side Menu Items-------------------");
+        for (WebElement item : admin_leftSideMenuItems) {
+            System.out.println(item.getText());
+        }
+        System.out.println("Total number of side menu options: " + admin_leftSideMenuItems.size());
+
+        admin_MenuOption.click(); 
+        admin_option1.click();
+		
+	}
+	public static void searchByUname(String uname) {
+		searchByUname_SearchBox.sendKeys("Admin");
+		searchByUname_searchBtn.click();
+		System.out.println("Search by Username '" + uname + "' found: " + resultRows.size() + " record(s).");
+		//driver.close();
+
+	}
+	
+	public static void searchbyRole() {
+		searchbyRole_userRoleDropdown.click();
+		searchbyRole_adminOption.click();
+		searchByUname_searchBtn.click();
+		System.out.println("Search by role,records found: " + resultRows.size() + " record(s).");
+        resetButton.click();
+        System.out.println("Page refreshed");
+        
+	}
+	
+	public static void searchByStatus(String statusText) {
+		searchByStatus_dropdown.click();
+		searchByStatus_enabled.click();
+		searchByUname_searchBtn.click();
+        System.out.println("Search by Status '" + statusText + " found: " + resultRows.size() + " record(s).");
+        resetButton.click();
+        System.out.println("Page refreshed");
+
+    }
+
+	
+	
+
+}
---
+++ OrangeHRMLogin.java
@@ -0,0 +1,160 @@
+package com.WebdriverDemo3;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.formula.functions.Rows;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class OrangeHRMLogin {
+	WebDriver driver;
+	Alert alt;
+    ChromeOptions options = new ChromeOptions();
+
+	
+	File filename;
+	FileInputStream fis;
+	XSSFWorkbook wb;
+	XSSFSheet xsheet;
+	XSSFRow rows;
+	XSSFCell cells;
+	int index = 0;
+	
+	@BeforeTest
+	  public void beforeTest() {
+
+		options.addArguments("--incognito");
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.setExperimentalOption("prefs", java.util.Map.of(
+                "credentials_enable_service", false,
+                "profile.password_manager_enabled", false
+            ));
+		  driver = new ChromeDriver(options);
+		  driver.manage().window().maximize();
+		  driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		  
+		  filename = new File("D:\\Akanksha-MyWorkspace3\\Hrm.xlsx");
+		  try {
+			  
+			fis = new FileInputStream(filename);//bcoz taking input from this file
+			wb = new XSSFWorkbook(fis);
+			xsheet = wb.getSheet("OrangeHRM");
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		  
+	  }
+	
+
+	@Test
+	public void d() throws InterruptedException, IOException {
+		  WebDriverWait Wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+		  xsheet = wb.getSheetAt(0);
+		  Thread.sleep(3000);
+		for(int i= 1; i<=xsheet.getLastRowNum();i++) {
+
+			rows = xsheet.getRow(i);
+			
+			String Uname = (rows.getCell(0) != null) ? rows.getCell(0).toString() : "";
+	        String Pswd = (rows.getCell(1) != null) ? rows.getCell(1).toString() : "";
+
+	        Wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+	        driver.findElement(By.name("username")).clear();
+	        driver.findElement(By.name("username")).sendKeys(Uname);
+	        driver.findElement(By.name("password")).clear();
+	        driver.findElement(By.name("password")).sendKeys(Pswd);
+	        driver.findElement(By.xpath("//*[text()=' Login ']")).click();
+	        
+	        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            takeSS("screenshots/LoginAttempt_" + Uname + "_" + timestamp + ".png");
+
+            if (Uname.equals("Admin") && Pswd.equals("admin123")) {
+                boolean loggedIn = driver.getCurrentUrl().contains("dashboard");
+                Assert.assertTrue(loggedIn, "Expected successful login for Admin.");
+                System.out.println("Valid login passed for: " + Uname +" " +Pswd);
+                logout();
+                
+            } else {
+            	
+            }  driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+            
+            if(!Uname.equals("Admin") && !Pswd.equals("admin123")) {
+            	System.out.println("Invalide attempt for :" +Uname+ " : " +Pswd );
+            	
+            } else {
+            	
+            }
+		}
+                
+            
+		}
+         
+		public void takeSS(String path) throws IOException, InterruptedException {
+	        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+	        Thread.sleep(2000);
+	        File dest = new File(path);
+	        dest.getParentFile().mkdirs(); // Ensure folder exists
+	        Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+	    }
+         
+         
+         public void logout() throws InterruptedException {
+ 	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+ 	        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("oxd-userdropdown-name")));
+ 	        driver.findElement(By.className("oxd-userdropdown-name")).click();
+ 	        Thread.sleep(3000);
+ 	        driver.findElement(By.xpath("//a[text()='Logout']")).click();
+ 	        Thread.sleep(1000);
+ 	    }
+	
+		
+	@AfterTest
+	  public void afterTest() throws IOException {
+		  FileOutputStream FOS = new FileOutputStream(filename); //to write the result value
+		  wb.write(FOS);
+		  wb.close();
+		  FOS.close();
+		  fis.close();
+		  driver.quit();
+		  
+		  
+
+}
+}
